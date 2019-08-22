9197fe1613a47d567337db5ee9269d074d69788a
simlf <lefournsimon@gmail.com>
2019-08-22 14:39:13 +0200

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :family, only: [:new, :destroy, :show, :edit, :update, :create]
+  resources :family
 
   resources :family, only: [:show] do
     resources :tasks
