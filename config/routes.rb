6ed65118de2d48094bd96c9ce1cc7f5be75e323e
nicolas goarant <nicolas.goarant@hotmail.fr>
2019-09-18 18:27:25 +0200

    family-bis

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { invitations: 'users/invitations', registrations: 'users/registrations' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ resource :family
+ resources :tasks
 end
