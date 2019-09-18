a7a7df29ee9087d66deb8092fb3348c2adb57450
simlf <lefournsimon@gmail.com>
2019-09-18 21:53:04 +0200

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ Rails.application.routes.draw do
   get 'family/show'
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
  # WARNING : don't forget to change this line to after development- resource :family, only: [:show], as: :current_profile do
 
-  resource :family, only: [:show], as: :current_profile do
+  resource :family, only: [:show]
     resources :tasks
 
   end
